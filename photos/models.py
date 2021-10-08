89abee69dac3b92e0a36c047138339737fa8d04b
Joe Gakumo <joegakumo1@gmail.com>
2021-10-08 14:26:51 +0300

    add class myphoto

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
 class Category(models.Model):
@@ -6,3 +7,8 @@ class Category(models.Model):
 
     def __str__(self):
      return self.name
+
+class    Myphoto(models.Model):
+    Category=models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image=models.ImageField(null=False, blank=False)
+    description=models.CharField(max_length=700, null=False, blank=False)
