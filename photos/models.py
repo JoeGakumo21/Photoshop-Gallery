22a8049c7a30869e13ecff04d0038a8f808248f6
Joe Gakumo <joegakumo1@gmail.com>
2021-10-08 14:29:36 +0300

    install pillow for images handler

--- photos/models.py
+++ photos/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-from django.db.models.expressions import F
+
 
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
-     return self.name
+      return self.name
 
 class    Myphoto(models.Model):
     Category=models.ForeignKey(Category, on_delete=models.SET_NULL)
     image=models.ImageField(null=False, blank=False)
     description=models.CharField(max_length=700, null=False, blank=False)
+
+    def __str__(self):
+        return self.description
