cfdaef3c4e2c2832e145da3be1cbe6004d0cb4cb
Joe Gakumo <joegakumo1@gmail.com>
2021-10-08 13:35:26 +0300

    added html structure

--- photos/urls.py
+++ photos/urls.py
@@ -4,6 +4,6 @@ from .import views
 
 urlpatterns=[
     path('',views.gallery, name='gallery'),
-    path('photo/<str:pk>/ ',views.viewPhoto, name='photo'),
+    path('photo/<str:pk>/',views.viewPhoto, name='photo'),
     path('add/',views.newPhoto, name='add')
 ]
